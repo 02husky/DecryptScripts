4894f05f1f2c2d4f9c72f1fbcb0934f1a590f653
2husky <790823951@qq.com>
2019-05-23 11:02:16 +0800

    Update Navicat.py

--- Navicat.py
+++ Navicat.py
@@ -1,41 +1,56 @@
-from winreg import OpenKey,HKEY_CURRENT_USER,EnumKey,EnumValue
+from winreg import OpenKey, HKEY_CURRENT_USER, EnumKey, EnumValue
 
-reg = r'Software\PremiumSoft\NavicatMSSQL\Servers'
-key = OpenKey(HKEY_CURRENT_USER,reg)
+regs = {'mssql': r'Software\PremiumSoft\NavicatMSSQL\Servers', 'mysql': r'Software\PremiumSoft\Navicat\Servers',
+        'oracle': r'Software\PremiumSoft\NavicatOra\Servers', 'pgsql': r'Software\PremiumSoft\NavicatPG\Servers',
+        'MariaDB': r'Software\PremiumSoft\NavicatMARIADB\Servers'}
 
-# 连接名
-conns = []
-try:
-    i = 0
-    while 1:
-        name = EnumKey(key,i)
-        conns.append(name)
-        i += 1
-except:
-    pass
 
-# 主机名
-hosts = []
-# 用户名
-usernames = []
-# 密码
-passwords = []
-for i in conns:
-    key = OpenKey(HKEY_CURRENT_USER,reg + '\\' + i)
+def get_info(dbname,reg):
+    key = OpenKey(HKEY_CURRENT_USER,reg)
+
+    # 连接名
+    conns = []
     try:
-        j = 0
+        i = 0
         while 1:
-            name, value, type = EnumValue(key, j)
-            if name == 'Host':
-                hosts.append(value)
-            if name == 'UserName':
-                usernames.append(value)
-            if name == 'Pwd':
-                passwords.append(value)
-            j += 1
+            name = EnumKey(key,i)
+            conns.append(name)
+            i += 1
     except:
         pass
 
-for i in range(len(hosts)):
-    with open('result.txt', 'a+') as f:
-        f.write(conns[i] + ' ' + hosts[i]  + ' ' + usernames[i] + ' ' + passwords[i] + '\n')
+    # 主机名
+    hosts = []
+    # 用户名
+    usernames = []
+    # 密码
+    passwords = []
+    for i in conns:
+        key = OpenKey(HKEY_CURRENT_USER,reg + '\\' + i)
+        try:
+            j = 0
+            while 1:
+                name, value, type = EnumValue(key, j)
+                if name == 'Host':
+                    hosts.append(value)
+                if name == 'UserName':
+                    usernames.append(value)
+                if name == 'Pwd':
+                    passwords.append(value)
+                j += 1
+        except:
+            pass
+    with open('result.txt', 'a') as f:
+        f.write('\n' + dbname + ' connections:' + '\n')
+    for i in range(len(hosts)):
+        with open('result.txt', 'a') as f:
+            f.write(' conn_name:' + conns[i] + '      ' + 'host_name:'+ hosts[i] + '      '
+                    + 'username:' + usernames[i] + '      ' + 'password:' + passwords[i] + '\n')
+
+
+if __name__ == '__main__':
+    for i, j in regs.items():
+        try:
+            get_info(i, j)
+        except:
+            continue
